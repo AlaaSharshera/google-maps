f21035c2dcd6acccec70e3acfd8e2277f92abb74
alaa <alaasharshera123@gmail.com>
2025-02-14 04:35:10 +0200

    change google map style

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ class TestGoogleMapsWithFlutter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      body: CustomGoogleMaps(),
-    ));
+          body: CustomGoogleMaps(),
+        ));
   }
 }
--- lib/widgets/custom_google_maps.dart
+++ lib/widgets/custom_google_maps.dart
@@ -10,15 +10,56 @@ class CustomGoogleMaps extends StatefulWidget {
 
 class _CustomGoogleMapsState extends State<CustomGoogleMaps> {
   late CameraPosition initialCameraPosition;
+  late GoogleMapController googleMapController;
   @override
   void initState() {
     initialCameraPosition = const CameraPosition(
-        target: LatLng(31.41821293734006, 31.812018125655563));
+        zoom: 14, target: LatLng(31.41821293734006, 31.812018125655563));
+
     super.initState();
   }
 
   @override
+  void dispose() {
+    googleMapController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GoogleMap(initialCameraPosition: initialCameraPosition);
+    return Stack(
+      children: [
+        GoogleMap(
+            onMapCreated: (controller) {
+              googleMapController = controller;
+              initMapStyle();
+            },
+            cameraTargetBounds: CameraTargetBounds(
+              LatLngBounds(
+                southwest: const LatLng(31.1, 31.5),
+                northeast: const LatLng(31.6, 32.0),
+              ),
+            ),
+            initialCameraPosition: initialCameraPosition),
+        Positioned(
+          bottom: 10,
+          right: 10,
+          left: 10,
+          child: ElevatedButton(
+            onPressed: () {
+              googleMapController.animateCamera(CameraUpdate.newLatLng(
+                  const LatLng(31.216006309265, 31.3613031634633)));
+            },
+            child: const Text("Change Location"),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void initMapStyle() async {
+    var nightMapStyle = await DefaultAssetBundle.of(context)
+        .loadString('assets/map_styles/map_night_style.json');
+    googleMapController.setMapStyle(nightMapStyle);
   }
 }
